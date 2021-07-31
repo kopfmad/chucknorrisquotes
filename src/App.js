a17e965a651a1bf3b44a7c64750fdbd6eb692414
andresantana <santana.agr@gmail.com>
2021-07-31 12:33:39 -0300

    exclamacao

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "../src/index.css";
 function App(props) {
   return (
     <div className="App">
-      <h1>App do Chuck Norris</h1>
+      <h1>App do Chuck Norris!</h1>
       {props.children}
     </div>
   );
