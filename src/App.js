4930c07890651ab7bea6e974140a2870320a5ce1
andresantana <santana.agr@gmail.com>
2021-07-31 13:00:06 -0300

    estrutura basica do app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import "../src/index.css";
 
 function App(props) {
-  return (
-    <div className="App">
-      <h1>App do Chuck Norris!</h1>
-      {props.children}
-    </div>
-  );
+  return <div className="App">{props.children}</div>;
 }
 
 export default App;
--- src/ComponentsAndStyles/Card.js
+++ src/ComponentsAndStyles/Card.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Card() {
-  return <div className="Card">Card</div>;
+  return (
+    <div className="Card">
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+        cupiditate repellat ad saepe voluptatem tempora velit quis expedita esse
+        sunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+        cupiditate repellat ad saepe voluptatem tempora velit quis expedita esse
+        sunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+        cupiditate repellat ad saepe voluptatem tempora velit quis expedita esse
+        sunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+        cupiditate repellat ad saepe voluptatem tempora velit quis expedita esse
+        sunt!
+      </h2>
+      <button>Aperte para mais quotes</button>
+    </div>
+  );
 }
