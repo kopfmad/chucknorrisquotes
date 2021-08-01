281a926c42d204f736e5125f972b389124e435f3
andresantana <santana.agr@gmail.com>
2021-08-01 14:39:38 -0300

    mensagem

--- src/ComponentsAndStyles/Card.js
+++ src/ComponentsAndStyles/Card.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 export default function Card() {
   let [texto, setTexto] = useState("Click to get quotes");
